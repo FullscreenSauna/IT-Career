00c43c179e4d15c47cb9b7bc1a29b70a868be408
Alexander Dzhekov <alexanderdz@abv.bg>
2019-02-08 11:21:51 +0200

    Distinct method learning

---
+++ Arrays and Lists/Array edit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_edit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArr = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Reverse")
+                {
+                    Array.Reverse(inputArr);
+                }
+
+                if (command == "Distinct")
+                {
+                    inputArr = inputArr.Distinct().ToArray();
+                }
+
+                if (command.Contains("Replace"))
+                {
+                    string[] replaceCommand = command.Split(' ').ToArray();
+                    int index = int.Parse(replaceCommand[1]);
+                    string replaceWord = replaceCommand[2];
+
+                    inputArr[index] = replaceWord;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", inputArr));
+        }
+    }
+}
---
+++ Arrays and Lists/Array safe edit/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_edit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArr = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            for (int i = 0; i < 5;)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Reverse")
+                {
+                    Array.Reverse(inputArr);
+                }
+
+                if (command == "Distinct")
+                {
+                   inputArr = inputArr.Distinct().ToArray();
+                }
+
+                if (command.Contains("Replace"))
+                {
+                    string[] replaceCommand = command.Split(' ').ToArray();
+                    int index = int.Parse(replaceCommand[1]);
+                    string replaceWord = replaceCommand[2];
+
+                    if (!(index > inputArr.Length - 1 || index < 0))
+                    {
+                        inputArr[index] = replaceWord;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input!");
+                    }
+                }
+
+                if (command == "END")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", inputArr));
+        }
+    }
+}
