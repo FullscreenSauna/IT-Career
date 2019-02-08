66e83d56b32080ab7324189899471c0aa0f07609
Alexander Dzhekov <alexanderdz@abv.bg>
2019-02-08 22:25:15 +0200

    Some easy exercises

---
+++ Arrays and Lists/Aray extract middle number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aray_extract_middle_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine().Split(' ').Where(x => x != "").Select(int.Parse).ToArray();
+
+            int lenght = inputArr.Length;
+
+            int firstNumber = 0;
+            int secondNumber = 0;
+            int thirdNumber = 0;
+
+            if (lenght == 1)
+            {
+                firstNumber = inputArr[0];
+
+                Console.WriteLine($"{firstNumber}");
+            }
+            else if (lenght % 2 == 0)
+            {
+                firstNumber = inputArr[lenght / 2 - 1];
+                secondNumber = inputArr[lenght / 2];
+
+                Console.WriteLine($"{firstNumber}, {secondNumber}");
+            }
+            else
+            {
+                firstNumber = inputArr[lenght / 2 - 1];
+                secondNumber = inputArr[lenght / 2];
+                thirdNumber = inputArr[lenght / 2 + 1];
+
+                Console.WriteLine($"{firstNumber}, {secondNumber}, {thirdNumber}");
+            }          
+        }
+    }
+}
---
+++ Arrays and Lists/Array last n digits/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_last_n_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] sequence = new int[n];
+
+            sequence[0] = 1;
+
+            int nextInSeq = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int stepBack = i;
+
+                if (i <= k)
+                {
+                    nextInSeq = sequence.Sum();
+                }
+                else
+                {
+                    nextInSeq = sequence[i - k];
+                    for (int j = 1; j < k; j++)
+                    {
+                        stepBack--;
+                        nextInSeq += sequence[stepBack];
+                    }
+                }
+
+                sequence[i] = nextInSeq;
+            }
+
+            Console.WriteLine(string.Join(" ", sequence));
+        }
+    }
+}
