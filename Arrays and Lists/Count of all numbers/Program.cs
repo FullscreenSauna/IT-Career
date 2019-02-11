987f4756aa49dc29061f5ee6a8d0d293c6961801
Alexander Dzhekov <alexanderdz@abv.bg>
2019-02-11 21:34:12 +0200

    Some more exercises

---
+++ Arrays and Lists/Count of all numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_of_all_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ').Where(x => x != "").Select(int.Parse).ToArray();
+
+            Array.Sort(numbers);
+
+            int count = 1;
+
+            //             2 2 2 2 3 7 8 8
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] == numbers[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i - 1]} -> {count}");
+                    count = 1;
+                }
+
+                if (i == numbers.Length -1)
+                {
+                    Console.WriteLine($"{numbers[i]} -> {count}");
+                }
+            }
+        }
+    }
+}
---
+++ Arrays and Lists/Square roots/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_roots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Where(x => x != "").Select(double.Parse).ToArray();
+
+            var result = new List<double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Math.Sqrt(input[i]) == (int)Math.Sqrt(input[i]))
+                {
+                    result.Add(input[i]);
+                }
+            }
+
+            result.Sort((a, b) => b.CompareTo(a));
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
