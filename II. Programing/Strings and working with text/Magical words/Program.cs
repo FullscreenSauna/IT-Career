b86d925b37665ea233221101a42ef070ddd667e7
Alexander Dzhekov <alexanderdz@abv.bg>
2019-03-03 15:46:34 +0200

    Return

---
+++ II. Programing/Strings and working with text/Magical words/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magical_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').ToArray();
+
+            var firstWord = input[0];
+            var secondWord = input[1];
+
+            bool isEquivalent = true;
+
+            if (firstWord.Length == secondWord.Length)
+            {
+                isEquivalent = SameWordLength(firstWord, secondWord, isEquivalent);
+            }
+            else
+            {
+                isEquivalent = DifferentWordLenght(firstWord, secondWord, isEquivalent);
+            }
+
+            Console.WriteLine(isEquivalent.ToString().ToLower());
+        }
+
+        private static bool SameWordLength(string firstWord, string secondWord, bool isEquivalent)
+        {
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                char currentLetter = firstWord[i];
+                char currentEquivalent = secondWord[i];
+
+                for (int j = 0; j < firstWord.Length; j++)
+                {
+                    char letterCheck = firstWord[j];
+                    char equivalentCheck = secondWord[j];
+
+                    if (currentLetter == letterCheck && currentEquivalent != equivalentCheck)
+                    {
+                        isEquivalent = false;
+                        break;
+                    }
+                }
+
+                if (!isEquivalent)
+                {
+                    break;
+                }
+            }
+
+            return isEquivalent;
+        }
+
+        private static bool DifferentWordLenght(string firstWord, string secondWord, bool isEquivalent)
+        {
+            var newFirstWord = new List<char>();
+            newFirstWord.AddRange(firstWord);
+            int firstWordCounter = 0;
+
+            while (newFirstWord.Count > 0)
+            {
+                char letter = newFirstWord[0];
+                newFirstWord.RemoveAll(x => x == letter);
+                firstWordCounter++;
+            }
+
+            var newSecondWord = new List<char>();
+            newSecondWord.AddRange(secondWord);
+            int secondWordCounter = 0;
+
+            while (newSecondWord.Count > 0)
+            {
+                char letter = newSecondWord[0];
+                newSecondWord.RemoveAll(x => x == letter);
+                secondWordCounter++;
+            }
+
+            if (firstWordCounter != secondWordCounter)
+            {
+                isEquivalent = false;
+            }
+
+            return isEquivalent;
+        }
+
+
+    }
+}
---
+++ II. Programing/Strings and working with text/Sum of big numbers/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_big_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstInput = Console.ReadLine();
+            var firstNum = new List<int>();
+            
+            for (int i = 0; i < firstInput.Length; i++)
+            {
+                firstNum.Add((int)Char.GetNumericValue(firstInput[i]));
+            }
+
+            var secondInput = Console.ReadLine();
+            var secondNum = new List<int>();
+
+            for (int i = 0; i < secondInput.Length; i++)
+            {
+                secondNum.Add((int)Char.GetNumericValue(secondInput[i]));
+            }
+
+            var endResult = new List<int>();
+            bool isBigger = false;
+
+            if (firstNum.Count > secondNum.Count || firstNum.Count == secondNum.Count)
+            {
+                isBigger = FirstNumLonger(firstNum, secondNum, endResult, isBigger);
+            }
+            else
+            {
+                isBigger = SecondNumLonger(firstNum, secondNum, endResult, isBigger);
+            }
+
+            if (endResult[endResult.Count -1] == 0)
+            {
+                endResult.Add(1);
+            }
+
+            endResult.Reverse();
+
+            Console.WriteLine(string.Join("",endResult));
+        }
+
+        private static bool FirstNumLonger(List<int> firstNum, List<int> secondNum, List<int> endResult, bool isBigger)
+        {
+            while (firstNum.Count > 0)
+            {
+                int result = 0;
+
+                if (isBigger)
+                {
+                    result++;
+                }
+
+                isBigger = false;
+                int num1 = firstNum[firstNum.Count - 1];
+
+                if (secondNum.Count > 0)
+                {
+                    int num2 = secondNum[secondNum.Count - 1];
+                    result += num1 + num2;
+                }
+                else
+                {
+                    result += num1;
+                }                
+
+                if (result > 9)
+                {
+                    isBigger = true;
+                    string seperateNums = result.ToString();
+
+                    endResult.Add((int)Char.GetNumericValue(seperateNums[1]));
+                }
+                else
+                {
+                    endResult.Add(result);
+                }
+
+                firstNum.RemoveAt(firstNum.Count - 1);
+
+                if (secondNum.Count > 0)
+                {
+                    secondNum.RemoveAt(secondNum.Count - 1);
+                }
+            }
+
+            return isBigger;
+        }
+
+        private static bool SecondNumLonger(List<int> firstNum, List<int> secondNum, List<int> endResult, bool isBigger)
+        {
+            while (secondNum.Count > 0)
+            {
+                int result = 0;
+
+                if (isBigger)
+                {
+                    result++;
+                }
+
+                isBigger = false;
+                int num1 = secondNum[secondNum.Count - 1];
+
+                if (firstNum.Count > 0)
+                {
+                    int num2 = firstNum[firstNum.Count - 1];
+                    result += num1 + num2;
+                }
+                else
+                {
+                    result += num1;
+                }
+
+                if (result > 9)
+                {
+                    isBigger = true;
+                    string seperateNums = result.ToString();
+
+                    endResult.Add((int)Char.GetNumericValue(seperateNums[1]));
+                }
+                else
+                {
+                    endResult.Add(result);
+                }
+
+                secondNum.RemoveAt(secondNum.Count - 1);
+
+                if (firstNum.Count > 0)
+                {
+                    firstNum.RemoveAt(firstNum.Count - 1);
+                }
+            }
+
+            return isBigger;
+        }
+
+
+    }
+}
