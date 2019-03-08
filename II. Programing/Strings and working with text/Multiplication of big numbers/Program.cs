92de1fc922aba16eed455de9c114a2c873020992
Alexander Dzhekov <alexanderdz@abv.bg>
2019-03-08 17:10:33 +0200

    I'm lazy

---
+++ II. Programing/Strings and working with text/Multiplication of big numbers/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_of_big_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumInput = Console.ReadLine();
+            var bigNum = new List<int>();
+
+            for (int i = 0; i < bigNumInput.Length; i++)
+            {
+                bigNum.Add((int)char.GetNumericValue(bigNumInput[i]));
+            }
+
+            int smallNum = int.Parse(Console.ReadLine());
+
+            var endResult = CalculateEndResult(bigNum, smallNum);
+
+            Console.WriteLine(string.Join("", endResult));
+        }
+
+        private static List<int> CalculateEndResult(List<int> bigNum, int smallNum)
+        {
+            var endResult = new List<int>();
+            int additional = 0;
+
+            while (bigNum.Count > 0)
+            {
+                int result = 0;
+
+                if (bigNum.Count == 1)
+                {
+                    result = smallNum * bigNum[0] + additional;
+                }
+                else
+                {
+                    result = smallNum * bigNum[bigNum.Count - 1] + additional;
+                }
+
+                if (result > 9)
+                {
+                    string newResult = result.ToString();
+
+                    endResult.Add((int)char.GetNumericValue(newResult[1]));
+                    additional = (int)char.GetNumericValue(newResult[0]);
+                }
+                else
+                {
+                    endResult.Add(result);
+                    additional = 0;
+                }
+
+                bigNum.RemoveAt(bigNum.Count - 1);
+            }
+
+            if (additional > 0)
+            {
+                endResult.Add(additional);
+            }
+
+            endResult.Reverse();
+            return endResult;
+        }
+    }
+}
---
+++ II. Programing/Strings and working with text/Numbers with suffixes and prefixes/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers_with_suffixes_and_prefixes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').ToArray();
+
+            double number = 0;
+            double endResult = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currentSet = input[i];
+
+                number = FindNumber(currentSet);
+
+                bool isPast = false;
+                bool isLowercase = false;
+
+                CalculateCurrentSetValue(ref number, currentSet, ref isPast, ref isLowercase);
+
+                endResult += number;
+            }
+
+            Console.WriteLine($"{endResult:f2}");
+        }
+
+        private static void CalculateCurrentSetValue(ref double number, string currentSet, ref bool isPast, ref bool isLowercase)
+        {
+            for (int j = 0; j < currentSet.Length; j++)
+            {
+                char currentChar = currentSet[j];
+
+                if (char.IsLower(currentChar))
+                {
+                    isLowercase = true;
+                }
+                else
+                {
+                    isLowercase = false;
+                }
+
+                if (char.IsDigit(currentChar))
+                {
+                    isPast = true;
+                }
+                else
+                {
+                    int positionInAlphabet = 0;
+
+                    if (isLowercase)
+                    {
+                        positionInAlphabet = currentChar - 96;
+                    }
+                    else
+                    {
+                        positionInAlphabet = currentChar - 64;
+                    }
+
+                    if (isPast)
+                    {
+                        if (isLowercase)
+                        {
+                            number += positionInAlphabet;
+                        }
+                        else
+                        {
+                            number -= positionInAlphabet;
+                        }
+                    }
+                    else
+                    {
+                        if (isLowercase)
+                        {
+                            number *= positionInAlphabet;
+                        }
+                        else
+                        {
+                            number /= positionInAlphabet;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private static double FindNumber(string currentSet)
+        {
+            double number;
+            var numberBuilder = new StringBuilder();
+
+            for (int j = 0; j < currentSet.Length; j++)
+            {
+                if (char.IsDigit(currentSet[j]))
+                {
+                    numberBuilder.Append(currentSet[j]);
+                }
+            }
+
+            number = double.Parse(numberBuilder.ToString());
+            return number;
+        }
+    }
+}
