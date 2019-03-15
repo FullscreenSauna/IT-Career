558f1b4b7a98cccc26208b82344b01654f5a9fc1
Alexander Dzhekov <alexanderdz@abv.bg>
2019-03-15 09:20:28 +0200

    Finish

---
+++ II. Programing/Dictionaries and Hash tables/Improved phonebook/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandArgs = Console.ReadLine().Split(' ');
+
+            var phonebook = new SortedDictionary<string, string>();
+            var outputs = new List<string>();
+
+            while (commandArgs[0] != "END")
+            {
+                if (commandArgs[0] == "A")
+                {
+                    string name = commandArgs[1];
+                    string number = commandArgs[2];
+
+                    phonebook[name] = number;
+                }
+                else if (commandArgs[0] == "S")
+                {
+                    string name = commandArgs[1];
+
+                    if (phonebook.ContainsKey(name))
+                    {
+                        outputs.Add($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        outputs.Add($"Contact {name} does not exist.");
+                    }
+                }
+                else if (commandArgs[0] == "ListAll")
+                {
+                    Console.WriteLine();
+
+                    foreach (var pair in phonebook)
+                    {
+                        Console.WriteLine($"{pair.Key} -> {pair.Value}");
+                    }
+                }
+
+                commandArgs = Console.ReadLine().Split(' ');
+            }
+
+            Console.WriteLine();
+
+            foreach (var output in outputs)
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ II. Programing/Dictionaries and Hash tables/Number of floating point numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_floating_point_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            var numbersDict = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!numbersDict.ContainsKey(number))
+                {
+                    numbersDict.Add(number, 1);
+                }
+                else
+                {
+                    numbersDict[number]++;
+                }
+            }
+
+            foreach (var pair in numbersDict)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
