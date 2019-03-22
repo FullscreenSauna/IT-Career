f468e80eb64d511b8ad16852f0dc6870ee80cbd5
Alexander Dzhekov <alexanderdz@abv.bg>
2019-03-22 11:21:53 +0200

    Email ex.

---
+++ II. Programing/Dictionaries and Hash tables/E-mail assignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_mail_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string oldInput = null;
+
+            var emails = new Dictionary<string, string>();
+
+            int i = 0;
+
+            while (input != "stop")
+            {
+                if (i % 2 == 0)
+                {
+                    emails.Add(input, null);
+                }
+                else
+                {
+                    emails[oldInput] = input;
+                }
+
+                oldInput = input;
+                i++;
+                input = Console.ReadLine();
+            }
+
+            foreach (var pair in emails)
+            {
+                if (pair.Value[pair.Value.Length - 1] != 'k' && pair.Value[pair.Value.Length - 1] != 's')
+                {
+                    Console.WriteLine($"{pair.Key} -> {pair.Value}");
+                }
+            }
+        }
+    }
+}
