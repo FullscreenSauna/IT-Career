94f4ef8ca2565776a742325e27d6e6598062b2b2
Alexander Dzhekov <alexanderdz@abv.bg>
2019-06-05 19:50:29 +0300

    Exam - full points

---
+++ III. Introduction to OOP/Exam/Online store/Order.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Online_Store
+{
+    public class Order
+    {
+        private int orderNumber;
+        private List<Product> products;
+
+        private static int ordersCount;
+
+        public int OrderNumber
+        {
+            get { return orderNumber; }
+            set { orderNumber = value; }
+        }
+
+        public static int OrdersCount
+        {
+            get { return ordersCount; }
+        }
+
+        public Order(int orderNumber, List<Product> products)
+        {
+            this.orderNumber = orderNumber;
+            this.products = products;
+            ordersCount++;
+        }
+  
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public double GetOrderTotalPrice()
+        {
+           return products.Sum(p => p.Price);
+        }
+
+        public double GetDiscountedProductsTotalPrice()
+        {
+            return products.Where(p => p.IsOnPromotion).Sum(p => p.Price);
+        }
+
+        public int GetDiscountedProductsCount()
+        {
+            return products.Count(p => p.IsOnPromotion);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"Order #{orderNumber} has the following products:");
+
+            products.ForEach(p => sb.Append($"\n###{p.ToString()}"));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ III. Introduction to OOP/Exam/Online store/Product.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Online_Store
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        private bool isOnPromotion;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Trim().Length < 3)
+                {
+                    throw new ArgumentException("Invalid product name!");
+                }
+
+                name = value;
+            }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0.00)
+                {
+                    throw new ArgumentException("Price should be positive!");
+                }
+
+                price = value;
+            }
+        }
+
+        public bool IsOnPromotion
+        {
+            get { return isOnPromotion; }
+        }
+
+        public Product(string name, double price, bool isOnPromotion)
+        {
+            this.Name = name;
+            this.isOnPromotion = isOnPromotion;
+
+            if (isOnPromotion)
+            {
+                Price = price * 0.8;
+            }
+            else
+            {
+                Price = price;
+            }
+        }
+
+        public Product(string name, double price) : this(name, price, false) { }
+
+        public override string ToString()
+        {
+            string message = "NO";
+
+            if (isOnPromotion)
+            {
+                message = "YES";
+            }
+
+            return $"Product -> {name} with price {price:f2}. On promotion {message}";
+        }
+    }
+}
