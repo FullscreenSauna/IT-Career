50f3c1e935b8e39e7b952644cd7d4375bb63d826
Alexander Dzhekov <alexanderdz@abv.bg>
2019-06-05 19:51:11 +0300

    Implementing an ArrayList

---
+++ IV. Introduction to ADS/Implementing ArrayList/MyArrayList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Implementing_ArrayList
+{
+    class MyArrayList<T>
+    {
+        private int capacity;
+        private T[] items;
+
+        public int Count { get; private set; }
+        public int Capacity { get => capacity; private set => capacity = value; }
+
+        public MyArrayList()
+        {
+            Capacity = 2;
+            items = new T[Capacity];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateIndex(index);
+                return this.items[index];
+            }
+
+            set
+            {
+                ValidateIndex(index);
+                this.items[index] = value;
+            }
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Check your indexes");
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Count >= capacity)
+            {
+                capacity *= 2;
+
+                T[] copy = new T[capacity];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    copy[i] = items[i];
+                }
+
+                items = copy;
+            }
+
+            items[Count] = item;
+            Count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ValidateIndex(index);
+
+            items[index] = default(T);
+
+            for (int i = index; i < Count - 1; i++)
+            {
+                items[i] = items[i + 1];
+            }
+
+            Count--;
+        }
+    }
+}
---
+++ IV. Introduction to ADS/Implementing ArrayList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrayList<int> test = new MyArrayList<int>();
+
+            test.Add(1);
+            test.Add(2);
+            test.Add(3);
+            test.Add(4);
+
+            test.RemoveAt(1);
+
+            test.Add(5);
+            Console.WriteLine(test[1]);
+        }
+    }
+}
