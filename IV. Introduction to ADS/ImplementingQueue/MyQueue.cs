c49004157d7c08955072206c9e572e0c6de3dbd6
Alexander Dzhekov <alexanderdz@abv.bg>
2019-06-12 12:42:07 +0300

    Implementing Queue

---
+++ IV. Introduction to ADS/ImplementingQueue/MyQueue.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingQueue
+{
+    class MyQueue<T>
+    {
+        internal class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            public Node(T value, Node<T> prevNode)
+            {
+                Value = value;
+                prevNode.Next = this;
+            }
+        }
+
+        public int Count { get; protected set; }
+        public Node<T> Head { get; protected set; }
+        public Node<T> Tail { get; protected set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                ValidateQueue();
+                return this[index];
+            }
+
+            set
+            {
+                ValidateQueue();
+                this[index] = value;
+            }
+        }
+
+        public MyQueue()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+
+        private void ValidateQueue()
+        {
+            if (Head == null)
+            {
+                throw new InvalidOperationException("Queue Empty");
+            }
+        }
+
+
+        public void Enque(T value)
+        {
+            Node<T> newNode;
+            if (Head == null)
+            {
+                newNode = new Node<T>(value);
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode = new Node<T>(value, Tail);
+                Tail = newNode;
+            }
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            ValidateQueue();
+
+            T value = default;
+            if (Head == Tail)
+            {
+                Count--;
+                value = Head.Value;
+                Head = Tail = null;
+                return value;
+            }
+
+            var currNode = Head;
+            for (int i = 0; i < Count - 1; i++)
+            {
+                if (currNode.Next == Tail)
+                {
+                    Count--;
+                    value = Tail.Value;
+                    currNode.Next = null;
+                    Tail = currNode;
+                    return value;
+                }
+                currNode = currNode.Next;
+            }
+
+            return default;
+        }
+
+        public T Peek()
+        {
+            ValidateQueue();
+            return Tail.Value;
+        }
+
+        public void Clear()
+        {
+            var currNode = Head;
+
+            while (Head != null)
+            {
+                currNode = currNode.Next;
+                Head = currNode;
+            }
+
+            Tail = Head;
+            Count = 0;
+        }
+
+        public bool Contains(T value)
+        {
+            ValidateQueue();
+
+            var currNode = Head;
+
+            for (int i = 0; i < Count; i++)
+            {
+                if (currNode.Value.Equals(value))
+                {
+                    return true;
+                }
+                currNode = currNode.Next;
+            }
+
+            return false;
+        }
+
+        public T[] ToArray()
+        {
+            ValidateQueue();
+
+            var arr = new T[Count];
+            var currNode = Head;
+
+            for (int i = 0; i < Count; i++)
+            {
+                arr[i] = currNode.Value;
+                currNode = currNode.Next;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ IV. Introduction to ADS/ImplementingQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementingQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MyQueue<int> queue = new MyQueue<int>();
+
+           Console.WriteLine(string.Join(", ", queue.ToArray()));
+        }
+    }
+}
